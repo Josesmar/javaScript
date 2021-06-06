cb291c70d7583ecd5219c0752da0b45b43b69ad2
Josesmar Santos <57329268+Josesmar@users.noreply.github.com>
2021-06-05 21:59:55 -0300

    Adicionando o descritivo do desafio

--- js/exibindoNumerosPares.js
+++ js/exibindoNumerosPares.js
@@ -1,3 +1,21 @@
+/*
+Desafios
+Crie um programa que leia um número e mostre os números pares até esse número, inclusive ele mesmo.
+
+Entrada
+Você receberá 1 valor inteiro N, onde N > 0.
+
+Saída
+Exiba todos os números pares até o valor de entrada, sendo um em cada linha. 
+
+ 
+Exemplo de Entrada	Exemplo de Saída
+6	
+2
+4
+6
+*/
+
 function functionChangenumerosPares(){
 N = parseFloat(gets());
 for (let num= 1; num <= N ; num++ ) {    
