4d40758b39254743ece36058bb449202b5b77b12
Josesmar <josesmargds@gmail.com>
2021-06-05 21:54:36 -0300

    Exibir números pares

---
+++ js/exibindoNumerosPares.js
@@ -0,0 +1,10 @@
+function functionChangenumerosPares(){
+N = parseFloat(gets());
+for (let num= 1; num <= N ; num++ ) {    
+
+    if ((num % 2) == 0) {
+        console.log(num);   
+      }     
+    }   
+}
+
